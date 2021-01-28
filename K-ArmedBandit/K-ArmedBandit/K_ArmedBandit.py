6df3255600370589f4e448b5b6cc9691eefe4644
Ethan Rathbun <ethanr2d2@gmail.com>
2021-01-27 23:02:02 -0500

    Added Additional Data Plotting

--- K-ArmedBandit/K-ArmedBandit/K_ArmedBandit.py
+++ K-ArmedBandit/K-ArmedBandit/K_ArmedBandit.py
@@ -14,38 +14,66 @@ class Bandit:
 		return np.random.normal(self.arms[arm], self.armStd)
 
 class EpsGreedy:
-	def __init__(self, numArms = 10, mean = 0, std = 1, armStd = 1, eps = .1):
-		self.bandit = Bandit(numArms, mean, std, armStd)
-		self.averages = np.zeros(numArms)
-		self.currentStep = 0
-		self.totalReward = 0
-		self.averageReward = None
-		self.eps = eps
+	#eps -> probability the algorithm chooses to explore a random arm rather than choosing the currently known best arm
+	def __init__(self, numArms = 10, mean = 0, std = 1, armStd = 1, eps = .1, learnRate = -1):
+		self.bandit = Bandit(numArms, mean, std, armStd) #the bandit
+		self.averages = np.zeros(numArms) #array containing currently approximated expected reward from each arm
+		self.actionCount = np.zeros(numArms) #keeps track of how many times each arm has been chosen
+		self.currentStep = 0 #current iteration
+		self.totalReward = 0 #cumulate model reward
+		self.averageReward = None #array containing average model reward as the model learns
+		self.averagsOverTime = None #array containing the approximated expected reward from each arm over time
+		self.eps = eps #probability the model chooses a random arm rather than the best known arm
+		self.learnRate = learnRate
 
 	def AdjustExpectedReward(self, arm, reward):
-		self.averages[arm] = self.averages[arm] + (1/self.currentStep)*(reward - self.averages[arm])
+		if self.learnRate <= 0:
+			self.averages[arm] = self.averages[arm] + (1/self.actionCount[arm])*(reward - self.averages[arm])
+		else:
+			self.averages[arm] = self.averages[arm] + self.learnRate*(reward - self.averages[arm])
 
 	def Learn(self, numIterations = 1000):
 		self.averageReward = np.zeros(numIterations)
+		self.averagesOverTime = np.zeros(shape = (numIterations, len(self.averages)))
 		while self.currentStep < numIterations:
 			if np.random.random() < self.eps:
 				action = np.random.randint(0,len(self.averages))
 			else:
 				action = np.argmax(self.averages)
 			reward = self.bandit.PullArm(action)
+
 			self.totalReward += reward
 			self.currentStep += 1
+			self.actionCount[action] += 1
+
 			self.averageReward[self.currentStep-1] = self.totalReward / self.currentStep
 			self.AdjustExpectedReward(action, reward)
+			self.averagesOverTime[self.currentStep-1] = self.averages
 
 	def PlotProgress(self):
+		maxReward = np.zeros(len(self.averageReward)) + np.max(self.bandit.arms)
 		plt.plot(self.averageReward, label = "Average: " + str(self.averageReward[-1]))
+		plt.plot(maxReward, label = "Average Reward From Optimal Arm")
 		plt.xlabel("Iterations")
 		plt.ylabel("Average Reward")
 		plt.title("Learning Progress of EpsGreedy Algorithm with Eps = " + str(self.eps))
 		plt.legend()
 		plt.show()
 
+	def PlotExpectedReward(self):
+		temp = np.transpose(self.averagesOverTime)
+		colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 
+			'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+		for i in range(len(self.averages)):
+			plt.plot(temp[i], label = "Estimated Expected Value of Arm " + str(i), color = colors[i])
+			plt.plot(self.bandit.arms[i], label = "Actual Average of Arm" + str(i), color = colors[i])
+		plt.xlabel("Iterations")
+		plt.ylabel("Average Reward")
+		plt.title("Model Attempting to Approximate Arm Rewards, Learn Rate: " + str(self.learnRate))
+		plt.legend()
+		plt.show()
+
 test = EpsGreedy()
 test.Learn()
-test.PlotProgress()
+#test.PlotProgress()
+test.PlotExpectedReward()
